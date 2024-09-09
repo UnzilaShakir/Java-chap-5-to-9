b86b3bfadce2b8902e4c3ca16bd9eb5b0c360e18
UnzilaShakir <unzilashakir21@gmail.com>
2024-09-10 04:07:59 +0500

    My Assignments

---
+++ app.js
@@ -0,0 +1,298 @@
+// Chap 5
+// Qno 1
+var num1 = 3
+var num2 = 5
+var num3 = 8
+document.write('Sum of' + " " + num1 + " " + 'and' + ' ' + num2 + " " + "is" + ' ' + num3 + '<br>')
+
+// Qno 2
+var num1 = 3
+var num2 = 5
+var num3 = 15
+document.write('Multiplication of of' + " " + num1 + " " + 'and' + ' ' + num2 + " " + "is" + ' ' + num3 + '<br>')
+
+var num1 = 3
+var num2 = 5
+var num3 = 2
+document.write('Subtraction of of' + " " + num1 + " " + 'and' + ' ' + num2 + " " + "is" + ' ' + num3 + '<br>')
+
+var num1 = 3
+var num2 = 5
+var num3 = 0.6
+document.write('Division of of' + " " + num1 + " " + 'and' + ' ' + num2 + " " + "is" + ' ' + num3 + '<br>')
+
+var num1 = 3
+var num2 = 5
+var num3 = 0.15
+document.write('Modulus of of' + " " + num1 + " " + 'and' + ' ' + num2 + " " + "is" + ' ' + num3 + '<br>' + '<br>')
+
+// Qno 3
+var num1 = 5 ;
+document.write('Value after variable declaration is' + ' ' + num1 + "<br>")
+
+var num1 = 5 ;
+document.write('Initial value is' + ' ' + num1 + "<br>")
+
+var num2 = 5
+
+// Qno 4
+
+var ticket = 600
+var num1 = 5
+var num2 = 600
+var num3 = 3000
+
+document.write('Total cost to buy' + ' ' + num1 + ' ' + 'tickets to a movie is' + ' ' + num3 + ' ' + 'PKR' + '<br>'  + '<br>')  
+
+// Qno 5
+
+document.write('Table of 4' + '<br>')
+
+var num1 = 4
+var num2 = 1
+document.write("4 x 1 = " +  num1*num2 + "<br>")
+var num1 = 4
+var num2 = 2
+document.write("4 x 2 = " +  num1*num2 + "<br>")
+var num1 = 4
+var num2 = 3
+document.write('4 x 3 =' +num1*num2 + '<br>')
+var num1 = 4
+var num2 = 4
+document.write('4 x 4 ='+ num1*num2 + '<br>')
+var num1 = 4
+var num2 = 5
+document.write('4 x 5 ='+ num1*num2 + '<br>')
+var num1 = 4
+var num2 = 6
+document.write('4 x 6 =' + num1*num2 + '<br>')
+var num1 = 4
+var num2 = 7
+document.write( '4 x 7 =' + num1*num2 + '<br>')
+var num1 = 4
+var num2 = 8
+document.write('4 x 8 ='+ num1*num2 + '<br>') 
+var num1 = 4
+var num2 = 9
+document.write('4 x 9 =' + num1*num2 + '<br>')
+var num1 = 4
+var num2 = 10
+document.write('4 x 10 =' + num1*num2 + '<br>' + '<br>')
+
+
+// QNO 7
+var priceofitem1 = 650
+var priceofitem2 = 100
+var quantityoftime1 = 3 
+var quantityofitem2 = 7
+var shippingcharges = 100
+document.write( 'Shopping Cart' + '<br>')
+document.write ('price of item 1 is 650' + '<br>')
+document.write('price of item 2 is 100' + '<br>')
+document.write ('shipping charges 100' + '<br>' + '<br>'+ '<br>')
+document.write('Total cost of your order is 2750'  + '<br>'+ '<br>')
+
+// QNO 8
+var totalmarks = 980
+var marksobtained = 804
+document.write (' totalmarks = 980' + '<br>')
+document.write (' marksobtained = 804' + '<br>')
+document.write('percentage:82.0408163265306%'  + '<br>' + '<br>')
+
+// QNO 9
+var UsDollar = 10
+var res1 = 10*104.80
+console.log(res1)
+
+var Riyals = 25
+var res2 = 25*28
+console.log(res2)
+
+// QNO 10
+var num1 = 2
+var num2 = 2 + 5 * 10 / 2
+
+console.log(2 + 5 * 10 / 2)
+
+// QNO 11
+var currentyear = 2024
+var birthyear = 2004
+document.write('current year=2024' + '<br>')
+document.write('birth year = 2004' + '<br>')
+document.write('your age is 20'  + '<br>'  + '<br>')
+
+// QNO 13
+var  currentage = 15
+var maxage = 65
+var amountofsnacks = 3
+
+document.write('fav snack = chocolatechip' + '<br>')
+document.write (' current age= 15' +'<br>')
+document.write('maximum age = 65' +'<br>')
+document.write('amount of snacks = 3' + '<br>')
+document.write('you will need 150 chocalate chip to last you until the ripe old age of 65' + '<br>'+ '<br>')
+
+// Chap 6
+// QNO 1
+var num1 = 10
+var num2 = ++num1
+console.log(num2)
+document.write('The value of a is 10' + '<br>')
+document.write('The value of ++a is 11' + '<br>')
+document.write('Now the value of a is 11' + '<br>' + '<br>')
+
+var num1 = 11
+var num2 = num1++
+console.log(num2)
+document.write('The value of a++ is 11' + '<br>')
+document.write('Now the value of a is 12' + '<br>' + '<br>')
+
+var num1 = 12
+var num2 = --num1
+console.log(num2)
+document.write('The value of --a is 11' + '<br>')
+document.write('Now the value of a is 11' + '<br>' + '<br>')
+
+var num1 = 11
+var num2 = num1--
+console.log(num2)
+document.write('The value of a-- is 11' + '<br>')
+document.write('Now the value of a is 10' + '<br>' + '<br>')
+
+// QNO 2
+var a = 2;
+var b = 1;
+var res = --a - --b + ++b + b--;
+// --a; = 1
+// --a - --b; = 1 -0 = 1
+// --a - --b + ++b; = 1 - 1 + 2 = 2
+// --a - --b + ++b + b--; 1 - 0 + 2 = 3
+console.log(res)
+
+
+// QNO 6
+var sub1 = "English"
+var sub2 = "Maths"
+var sub3 = "Urdu"
+
+var num1 = 100
+var num2 = 100
+var num3 = 100
+
+var num1 = 54
+var num2 = 54
+var num3 = 48
+
+// Chap 7
+// QNO 1
+var num1 = 2
+var num2 = 6
+var res = 2 + (2 * 6);
+var res2 = 14
+console.log(res)
+
+// QNO 2
+var num1 = 2
+var num2 = 6
+var res = (2 + 2) * 6;
+var res2 = 24
+console.log(res)
+
+// QNO 3
+var num1 = 2
+var num2 = 4
+var res = (2 + 2) * (4 + 2);
+var res2 = 24
+console.log(res)
+
+// QNO 4
+var num1 = 2
+var num2 = 4
+var res = ((2 + 2) * 4) + 2;
+var res2 = 18
+console.log(res)
+
+// Chap 8
+// QNO 1
+var num = "2" + "2";
+var res = 22
+
+// QNO 2
+alert("Hello" + "Dolly");
+
+// QNO 3
+alert("33" + 3)
+
+// QNO 4
+alert("Pakistan" + "Zindabad")
+
+// QNO 5
+
+var num1 = "15"
+var num2 = "24"
+var res = "1524"
+console.log(num1+num2)
+
+// QNO 6
+var num1 = "45"
+var num2 =  "56"
+var num3 = "4556"
+console.log(num1+num2)
+
+// Chap 9
+// QNO 1
+prompt("Enter First name")
+
+// QNO 2
+prompt("Country" , "China")
+
+// QNO 3
+var YourName = prompt("Enter Your Name")
+
+// QNO 4
+prompt("Enter Your  Email address" , "Required") 
+
+// QNO 5
+// Assign strings to two variables
+var message = "Please enter your name:";
+var defaultResponse = "John Doe";
+
+// Display the prompt and assign the user's response to a third variable
+var userResponse = prompt(message, defaultResponse);
+
+// QNO 6
+    // Display a prompt with a message and a default response
+    var userResponse = prompt("Please enter your name:", "Mr David");
+
+    // Display the user's response in an alert
+    alert("You entered: " + userResponse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
